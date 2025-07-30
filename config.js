c742e034a220a3badc2b7dcfd0bb0069d4f9e823
Jacopo Colombo <jakkolo@gmail.com>
2025-07-30 10:24:12 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // config.js
 
 // --- JSONBin.io Configuration ---
-export const JSONBIN_MASTER_KEY = '$2a$10$moQg0NYbmqEkIUS1bTku2uiW8ywvcz0Bt8HKG3J/4qYU8dCZggiT6'; // LA TUA MASTER KEY!
+export const JSONBIN_MASTER_KEY = '$2a$10$moQg0NYbmqEkIUS1bTku2uiW8ywvcz0Bt8HKG3J/4qYU8dCZggiT6'; // MASTER KEY!
 
 // Bin ID dedicato agli EVENTI
 export const JSONBIN_EVENTS_BIN_ID = '68870d4d7b4b8670d8a868e8'; // Il tuo Bin ID per gli eventi
@@ -13,4 +13,4 @@ export const JSONBIN_LOGS_BIN_ID = '688924c7f7e7a370d1eff96b'; // Logs Bin ID
 export const JSONBIN_LOGS_WRITE_URL = `https://api.jsonbin.io/v3/b/${JSONBIN_LOGS_BIN_ID}`;
 
 // Altre costanti globali
-export const NOMINATIM_USER_AGENT = 'LacrosseEventApp/1.0 (your-email@example.com)'; // CAMBIA QUESTO!
+export const NOMINATIM_USER_AGENT = 'LacrosseEventApp/1.0 (jakkolo@gmail.com)'; 
